90df279291b2fc032342e97c1a6c58f476ff8661
DevashishChoudhary <isshuchoudhary99@gmail.com>
2020-11-20 01:47:11 +0530

    First Test Commit

---
+++ maven-demo-project/src/test/java/com/Devashish/Choudhary/Test_Class.java
@@ -0,0 +1,13 @@
+package com.Devashish.Choudhary;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Test_Class {
+
+	@Test
+	public void test() {
+		Assert.assertEquals("Hello, World", new String("Hello, World"));
+	}
+}
+
